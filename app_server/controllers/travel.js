fe3cf7bbf89ac6a26c2b78ec812d2fc297e9ee0b
Yoel Marquez <yoel7501@gmail.com>
2025-04-20 04:31:41 -0400

    module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('/CS-465/travlr/data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
-    res.render('travel', { title: "Travlr Getaways"});
+    res.render('travel', {title: 'Travel Getaways', trips});
 };
 
 module.exports = {
