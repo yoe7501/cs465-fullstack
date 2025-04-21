b8d02ad5db58ce72caeb847dd2f5b3e0bae3df5a
Yoel Marquez <yoel7501@gmail.com>
2025-04-20 21:33:38 -0400

    Module 6 completed baseline

---
+++ app_admin/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule, TripListingComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Travlr Getaways Admin';
+}
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -24,24 +24,96 @@ const tripsList = async (req, res) => {
   }
 };
 
-const tripsFindByCode = async(req,res) => {
+const tripsFindByCode = async (req, res) => {
   const q = await Model
-      .find({'code' : req.params.tripCode})
-      .exec()
+    .find({ 'code': req.params.tripCode })
+    .exec()
 
-  if(!q)
-  {
+  if (!q) {
     return res
-            .status(404)
-            .json(err);
+      .status(404)
+      .json(err);
   } else {
-      return res
-          .status(200)
-          .json(q);
+    return res
+      .status(200)
+      .json(q);
   }
 };
 
+// POST: /trips - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+  const newTrip = new Trip({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description
+  });
+
+  const q = await newTrip.save();
+
+  if (!q) { // Database returned no data
+    return res
+      .status(400)
+      .json(err);
+  } else { // Return new trip
+    return res
+      .status(201)
+      .json(q);
+  }
+
+  // Uncomment the following line to show results of operation
+  // on the console
+  // console.log(q);
+};
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status
+code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+  // Uncomment for debugging
+  console.log(req.params);
+  console.log(req.body);
+  const q = await Model
+    .findOneAndUpdate(
+      { 'code': req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+      }
+    )
+    .exec();
+  if (!q) { // Database returned no data
+    return res
+      .status(400)
+      .json(err);
+  } else { // Return resulting updated trip
+    return res
+      .status(201)
+      .json(q);
+  }
+  // Uncomment the following line to show results of
+  operation
+  // on the console
+  // console.log(q);
+};
+
+
 module.exports = {
   tripsList,
-  tripsFindByCode
+  tripsFindByCode,
+  tripsAddTrip,
+  tripsUpdateTrip
 };
